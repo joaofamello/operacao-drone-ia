5dd4b2d499582b485222bcf30ca9ef1f1e7ec91c
João Francisco <joaofrancisco.amello@gmail.com>
2026-07-18 13:41:56 -0300

    feat(environment.py): definição do método que espalha o fogo

--- environment.py
+++ environment.py
@@ -22,69 +22,90 @@ class OperacaoDrone:
         self.grid[self.fogo_pos[0]][self.fogo_pos[1]] = 2
         self.grid[self.drone_pos[0]][self.drone_pos[1]] = 1
 
-        def renderizar(self):
-            if os.name == 'nt': # Windows
-                subprocess.run(['cls'], shell=True)
-            else: # Linux/macOS
-                subprocess.run(['clear'])
+    def renderizar(self):
+        if os.name == 'nt': # Windows
+            subprocess.run(['cls'], shell=True)
+        else: # Linux/macOS
+            subprocess.run(['clear'])
 
-            # Códigos ANSI para colorir o fundo (Background)
-            RESET = "\033[0m"
-            BG_CINZA = "\033[100m"  # Terra queimada
-            BG_VERDE = "\033[42m"  # Floresta
-            BG_VERMELHO = "\033[41m"  # Fogo
-            BG_AZUL = "\033[44m"  # Lago
-            BG_BRANCO = "\033[47m"  # Fundo do Drone
-            TXT_PRETO = "\033[30m"  # Texto do Drone
+        # Códigos ANSI para colorir o fundo (Background)
+        RESET = "\033[0m"
+        BG_CINZA = "\033[100m"  # Terra queimada
+        BG_VERDE = "\033[42m"  # Floresta
+        BG_VERMELHO = "\033[41m"  # Fogo
+        BG_AZUL = "\033[44m"  # Lago
+        BG_BRANCO = "\033[47m"  # Fundo do Drone
+        TXT_PRETO = "\033[30m"  # Texto do Drone
 
-            print("=== OPERAÇÃO DRONE ===")
-            print(f"Nível de Água: {self.agua_maxima}")
-            print("Controles: [W] Cima | [S] Baixo | [A] Esquerda | [D] Direita\n")
-            print("Ações: [E] Jogar Água | [R] Reabastecer | [Q] Sair\n")
+        print("=== OPERAÇÃO DRONE ===")
+        print(f"Nível de Água: {self.agua_maxima}")
+        print("Controles: [W] Cima | [S] Baixo | [A] Esquerda | [D] Direita\n")
+        print("Ações: [E] Jogar Água | [R] Reabastecer | [Q] Sair\n")
 
-            for i in range(self.tamanho):
-                linha = ""
-                for j in range(self.tamanho):
-                    if [i, j] in self.drone_pos:
-                        linha += f"{BG_BRANCO}{TXT_PRETO} D {RESET}"
-                    else:
-                        estado_celula = self.grid[i][j]
-                        if estado_celula == 0:
-                            linha += f"{BG_CINZA}\t{RESET}"
-                        elif estado_celula == 1:
-                            linha += f"{BG_VERDE}\t{RESET}"
-                        elif estado_celula == 2:
-                            linha += f"{BG_VERMELHO}\t{RESET}"
-                        elif estado_celula == 3:
-                            linha += f"{BG_AZUL}\t{RESET}"
+        for i in range(self.tamanho):
+            linha = ""
+            for j in range(self.tamanho):
+                if [i, j] in self.drone_pos:
+                    linha += f"{BG_BRANCO}{TXT_PRETO} D {RESET}"
+                else:
+                    estado_celula = self.grid[i][j]
+                    if estado_celula == 0:
+                        linha += f"{BG_CINZA}\t{RESET}"
+                    elif estado_celula == 1:
+                        linha += f"{BG_VERDE}\t{RESET}"
+                    elif estado_celula == 2:
+                        linha += f"{BG_VERMELHO}\t{RESET}"
+                    elif estado_celula == 3:
+                        linha += f"{BG_AZUL}\t{RESET}"
 
-                print(linha)
-            print("\n"+"="*22)
+            print(linha)
+        print("\n"+"="*22)
 
-        def mover_drone(self, acao):
-            linha, col = self.drone_pos
+    def mover_drone(self, acao):
+        linha, col = self.drone_pos
 
-            if acao == 'w' and linha > 0:
-                self.drone_pos[0] -= 1
-            elif acao == 's' and linha < self.tamanho - 1:
-                self.drone_pos[0] += 1
-            elif acao == 'a' and col > 0:
-                self.drone_pos[1] -= 1
-            elif acao == 'd' and col < self.tamanho - 1:
-                self.drone_pos[1] += 1
+        if acao == 'w' and linha > 0:
+            self.drone_pos[0] -= 1
+        elif acao == 's' and linha < self.tamanho - 1:
+            self.drone_pos[0] += 1
+        elif acao == 'a' and col > 0:
+            self.drone_pos[1] -= 1
+        elif acao == 'd' and col < self.tamanho - 1:
+            self.drone_pos[1] += 1
 
-            elif acao == 'e': # Jogar água
-                if self.agua_atual > 0 and self.grid[linha][col] == 2:
-                    self.grid[linha][col] = 1 # Fogo apagado, volta a ser floresta
-                    self.agua_atual -= 1
-                    print("\n[!] Fogo apagado!")
-                    time.sleep(1)
-                elif self.agua_atual == 0:
-                    print("\n[X] Sem água! Abasteça no lago.")
-                    time.sleep(1)
+        elif acao == 'e': # Jogar água
+            if self.agua_atual > 0 and self.grid[linha][col] == 2:
+                self.grid[linha][col] = 1 # Fogo apagado, volta a ser floresta
+                self.agua_atual -= 1
+                print("\n[!] Fogo apagado!")
+                time.sleep(1)
+            elif self.agua_atual == 0:
+                print("\n[X] Sem água! Abasteça no lago.")
+                time.sleep(1)
 
-            elif acao == 'r': # Reabastecer
-                if self.grid[linha][col] == 3:
-                    self.agua_atual = self.agua_maxima
-                    print("\n[!] Tanque cheio!")
-                    time.sleep(1)
+        elif acao == 'r': # Reabastecer
+            if self.grid[linha][col] == 3:
+                self.agua_atual = self.agua_maxima
+                print("\n[!] Tanque cheio!")
+                time.sleep(1)
+
+    def espalhar_fogo(self):
+        novos_fogos = []
+        for i in range(self.tamanho):
+            for j in range(self.tamanho):
+                if self.grid[i][j] == 2: # Se tem fogo
+                    # Tenta alastrar o fogo para uma célula adjacente
+                    direcoes = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                    random.shuffle(direcoes)
+                    for dl, dc in direcoes:
+                        nl, nc = i + dl, j + dc
+                        # Se estiver dentro do mapa e for florsta (1)
+                        if 0 <= nl < self.tamanho and 0 <= nc < self.tamanho:
+                            if self.grid[nl][nc] == 1:
+                                if random.random() < 0.2: # 20% de chance de espalhar.
+                                    novos_fogos.append((nl, nc))
+                                break
+
+        for l, c in novos_fogos:
+            self.grid[l][c] = 2
+
